716998a91f417d4ba23a3ac89ce371db84b51c68
Stepkata <katstep@student.agh.edu.pl>
2023-11-20 19:25:18 +0100

    game

---
+++ game.py
@@ -0,0 +1,286 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+from world_generator import generateWorld
+import pickle
+import math
+import numpy as np
+from collections import deque
+
+pygame.init()
+#font = pygame.font.Font('arial.ttf', 25)
+font = pygame.font.SysFont('arial', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+    
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+POISON = (200,0,0)
+EADIBLE = (247, 149, 45)
+
+BLOCK_SIZE = 6
+SPEED = 5
+
+class AIGame:
+    
+    def __init__(self, w=1400, h=750, seed=890):
+        #display params
+        self.width = w
+        self.height = h
+        self.w = 200
+        self.h = 120
+        self.board_start = 50
+        self.tile = 20
+
+        # init display
+        self.display = pygame.display.set_mode((self.width, self.height))
+        self.game_board = pygame.surface.Surface((self.w*BLOCK_SIZE, self.h*BLOCK_SIZE)).convert()
+        self.stats_board = pygame.surface.Surface((250, h)).convert()
+        pygame.display.set_caption('AIGame')
+        self.clock = pygame.time.Clock()
+        
+        # init game state
+        self.direction = Direction.RIGHT
+
+        #snake
+        self.head = Point(int(self.w/2), int(self.h/2))
+        self.player = [self.head]
+            
+        self.score = 0
+        self.stats = deque(maxlen=20)
+
+        self.hunger = 30
+        self.wait = False
+        self.food_eadible = []
+        self.food_poison = []
+        self.seed = seed
+        self._spawn_food_locations = []
+        self.frame_iteration = 0
+        self.world = generateWorld(self.w, self.h, seed)
+        self._create_food()
+
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.player = [self.head]
+
+        self.score = 0
+        self.food_eadible = []
+        self.food_poison = []
+        self._create_food()
+        self.frame_iteration = 0
+    
+    def _create_food(self):
+        for i in range(int(self.w/self.tile)):
+            for j in range(int(self.h/self.tile)):           
+                for _ in range(5):
+                    self._place_food(i, j, self.food_eadible)
+                for _ in range(2):
+                    self._place_food(i, j, self.food_poison)
+
+
+    def _place_food(self, w, h, food_array):
+        x = random.randint(w*self.tile, (w+1)*self.tile-1)
+        y = random.randint(h*self.tile, (h+1)*self.tile-1)
+        (_, name) = self.world[(x, y)]
+        if name == 'water': #no food in water
+            return
+        elif name == 'swamp': #more food on swamps
+            n = random.randint(0, 100)
+            if n < 7:
+                self._place_food(w,h,food_array)
+        elif name == 'forest': #much more food in the forest
+            n = random.randint(0, 100)
+            if n < 15:
+                self._place_food(w,h,food_array)
+        food_array.append(Point(x, y))
+
+    def nearest_food(self, sight=1, pt=None):
+        look = np.zeros((sight*2+1, sight*2+1))
+        if pt is None:
+            pt = self.head
+        for x in range(pt.x - sight, pt.x+sight):
+            for y in range(pt.y-sight, pt.y + sight):
+                look[x,y] = ((x,y) in self.food_eadible)
+        return look      
+
+    def play_step(self, action):
+        self.frame_iteration += 1
+        reward = 0
+       
+        self.wait = not(self.wait)
+        if self.clock.get_time() % 40 == 0:
+            for spawn in self._spawn_food_locations:
+                self._place_food(spawn.x, spawn.y, self.food_eadible)
+            self._spawn_food_locations.clear()
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
+        
+        
+        (_, name) = self.world[(self.head.x, self.head.y)]
+        if name == "swamp" and self.wait:
+            pass
+        else:
+            # 2. move
+            self._move(action) # update the head
+            self.player.insert(0, self.head)
+            self.player.pop()
+        
+        game_over = False
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+
+            
+        # 4. eat food or just move
+        if self.head in self.food_eadible:
+            self.score += 1
+            reward = 10
+            self.hunger += 7
+            self.food_eadible.remove(self.head)
+            self._spawn_food_locations.append(Point(math.floor(self.head.x/self.tile), math.floor(self.head.y/self.tile)))
+        
+        self.stats.append(self.score)
+        # 5. update ui and clock
+        self._update_ui()
+        self.hunger -= 1
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return reward, game_over, self.score
+    
+    def _is_collision(self, pt= None):
+        # hits boundary
+        if pt is None:
+            pt = self.head
+        if pt.x > self.w or pt.x < 0 or pt.y > self.h - 1 or pt.y < 0:
+            print("Walked into the electric fence!")
+            return True
+        
+        if self.hunger <= 0:
+            print("Starved to death!")
+            return True
+
+        #eats poisoned food
+        if pt in self.food_poison:
+            self.hunger += 5
+            print("Ate poison!")
+            return True
+        #drowns
+        (_, name) = self.world[(pt.x, pt.y)]
+        if name == "water":
+            print("Drowned!")
+            return True
+        #gets killed by a predator
+        elif name == "forest":
+            x = random.randint(0, 100)
+            if x < 3:
+                print("Eaten by the predators")
+                return True
+        
+        return False
+        
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        self.drawBackground()
+        self.drawBoard()
+        self.display.blit(self.game_board, [self.board_start, self.board_start])
+
+        self.draw_stats()
+        self.display.blit(self.stats_board, [self.width-260, 0])
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [self.width-200, 0])
+        text2 = font.render("Hunger: " + str(self.hunger), True, WHITE)
+        self.display.blit(text2, [self.width-200, 40])
+        pygame.display.update()
+        pygame.display.flip()
+
+    def drawBoard(self):
+        
+        for pt in self.player:
+            (xp, yp) = (pt.x*BLOCK_SIZE,pt.y*BLOCK_SIZE)
+            pygame.draw.rect(self.game_board, BLUE1, pygame.Rect(xp, yp, BLOCK_SIZE, BLOCK_SIZE))
+
+        for food in self.food_eadible:    
+            (xp, yp) = (food.x*BLOCK_SIZE,food.y*BLOCK_SIZE)
+            pygame.draw.rect(self.game_board, EADIBLE, pygame.Rect(xp, yp, BLOCK_SIZE, BLOCK_SIZE))
+
+        for food in self.food_poison:    
+            (xp, yp) = (food.x*BLOCK_SIZE,food.y*BLOCK_SIZE)
+            pygame.draw.rect(self.game_board, POISON, pygame.Rect(xp, yp, BLOCK_SIZE, BLOCK_SIZE))
+
+    def drawBackground(self):
+        for (x, y), ((r,g,b,_), _) in self.world.items():
+                pygame.draw.rect(self.game_board, (r*255,g*255,b*255), pygame.Rect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+        
+        for x in range(0,   self.w*BLOCK_SIZE, BLOCK_SIZE):
+            pygame.draw.line(self.game_board, (255, 255, 255, 0), (x, 0), ( x,  self.h*BLOCK_SIZE))
+        for y in range(0,  self.h*BLOCK_SIZE, BLOCK_SIZE):
+            pygame.draw.line(self.game_board, (255, 255, 255, 20), (0, y), ( self.w*BLOCK_SIZE, y))   
+
+    def draw_stats(self):
+        self.stats_board.fill(BLACK)
+        pygame.draw.line(self.stats_board, (255, 255, 255, 0), (30, 100), ( 30,  255))
+        pygame.draw.line(self.stats_board, (255, 255, 255, 0), (25, 250), ( 230,  250))
+        text = font.render(str(max(self.stats)), True, WHITE)
+        self.stats_board.blit(text, [10, 70])
+        self.stats_board.blit(font.render(str(0), True, WHITE), [10, 250])
+        for i in range(len(self.stats)-2):
+            h1 = 250 - 150*((self.stats[i]+1)/(max(self.stats)+1))
+            h2 = 250 - 150*((self.stats[i+1]+1)/(max(self.stats)+1))
+            pygame.draw.line(self.stats_board, (255, 255, 255, 0), 
+                             (30+i*10, h1), ( 30+(i+1)*10,  h2))
+
+
+        
+    def _move(self, action):
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clock_wise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clock_wise[idx] # no change
+        elif np.array_equal(action, [0, 1, 0]):
+            next_idx = (idx + 1) % 4
+            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        else: # [0, 0, 1]
+            next_idx = (idx - 1) % 4
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+
+        self.direction = new_dir
+
+        self.direction = new_dir
+        x = self.head.x
+        y = self.head.y
+        if self.direction == Direction.RIGHT:
+            x += 1
+        elif self.direction == Direction.LEFT:
+            x -= 1
+        elif self.direction == Direction.DOWN:
+            y += 1
+        elif self.direction == Direction.UP:
+            y -= 1
+            
+        self.head = Point(x, y)
+
